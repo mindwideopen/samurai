74471f890a8b10384551deb2ea675cfa76d988d3
unknown <nlit3926@gmail.com>
2023-09-18 19:03:33 +0200

    ne idet

---
+++ src/redux/state.js
@@ -0,0 +1,26 @@
+let posts = [
+    {id: 1, message:"Hi how are you", likesCount: 1},
+    {id: 2, message:"Im OK", likesCount: 6},
+    {id: 2, message:"Almost got it!", likesCount: 100},
+    {id: 2, message:"Very close", likesCount: 1000}
+]
+
+let messagesData = [
+    {id: 1, message: 'Hi!'},
+    {id: 2, message: 'How u doin'},
+    {id: 3, message: 'How are u doin'},
+    {id: 4, message: 'How were u doin'},
+    {id: 5, message: 'How will u be doin'}
+]
+
+let dialogsData = [
+    {id: 1, name: 'Lera'},
+    {id: 2, name: 'Viktor'},
+    {id: 3, name: 'Lex'},
+    {id: 4, name: 'Dasha'},
+    {id: 5, name: 'Korzh'},
+
+]
+let state = [posts, messagesData, dialogsData]
+
+export default State
