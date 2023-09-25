043bb5185d6c86d30dee57166b7dbd8b71995ea4
Nikolay <nlit3926@gmail.com>
2023-09-25 19:37:20 +0200

    Test commit for all files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import Friends from "./components/Friends/Friends";
-import {updatePostText} from "./redux/state";
+
 
 
 const App = (props) => {
