4549a8d378a792901329baf53aa544edc842c7be
Nikolay <nlit3926@gmail.com>
2023-10-25 16:26:15 +0200

    Пушу в Master

--- src/User/User.jsx
+++ src/User/User.jsx
@@ -3,12 +3,8 @@ import React from "react";
 export const User = () => {
     return (
         <div>
-            User
-            User
-            User
-            User
-            User
-            Before Remote commit
+
+            Test 111
         </div>
     )
 }
